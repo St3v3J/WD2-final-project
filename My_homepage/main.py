88d07cc241d6fc787811a1c64b1b9feeddfbd260
Steven Johnson <stevendgjohnson@gmail.com>
2020-05-13 18:50:48 -0500

    GuessingGame_Heroku

--- My_homepage/main.py
+++ My_homepage/main.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+
+
+@app.route("/contract", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 
 @app.route("/")
